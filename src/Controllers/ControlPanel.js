4b3b4ca6332943517bd2f3c835376be87705e3f2
Skarrez <166002048+Skarrez@users.noreply.github.com>
2024-05-02 10:24:58 +0200

    Update ControlPanel.js max fps 300000 and if >= 500000

--- src/Controllers/ControlPanel.js
+++ src/Controllers/ControlPanel.js
@@ -122,9 +122,9 @@ class ControlPanel {
     defineEngineSpeedControls(){
         this.slider = document.getElementById("slider");
         this.slider.oninput = function() {
-            const max_fps = 100000;
+            const max_fps = 300000;
             this.fps = this.slider.value;
-            if (this.fps>=max_fps) this.fps = 1000;
+            if (this.fps>=max_fps) this.fps = 500000;
             if (this.engine.running) {
                 this.changeEngineSpeed(this.fps);
             }
